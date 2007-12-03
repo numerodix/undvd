1a18b096f5bfb10931d00c20f8c7d5876b8dfefa
numerodix <numerodix@gmail.com>
2007-12-03 22:40:43 +0100

    checking in 0.2.0

---
+++ lib.sh
@@ -0,0 +1,40 @@
+# Author: Martin Matusiak <numerodix@gmail.com>
+# Licensed under the GNU Public License, version 3.
+
+### DECLARATIONS
+
+# undvd version
+version=0.2.0
+
+tmpdir="/tmp"
+
+# colors
+wh="\033[1;37m"
+pl="\033[m"
+ye="\033[1;33m"
+cy="\033[1;36m"
+gr="\033[1;32m"
+re="\033[1;31m"
+
+# x264 encoding options
+x264="x264 -x264encopts subq=5:frameref=2:partitions=all:weight_b:bitrate=900:threads=auto"
+faac="faac -faacopts object=1:tns:quality=100"
+
+# xvid encoding options
+xvid="xvid -xvidencopts bitrate=900"
+lame="mp3lame -lameopts vbr=2:q=3"
+
+# codec defaults
+vcodec=$x264
+acodec=$lame
+
+# mplayer filters
+prescale=
+postscale=
+
+# sources
+disc_image="disc.iso"
+mencoder_source="$disc_image"
+
+# seconds to pause between updating rip status line
+timer_refresh=5
