383409a2d8a610ab22b21eb683af4dd682957c1e
Martin Matusiak <numerodix@gmail.com>
2008-03-01 02:08:06 +0100

    emphasize red color on xterm terminals make progress indicator more visible

--- colors.sh
+++ colors.sh
@@ -1,7 +1,6 @@
 # Author: Martin Matusiak <numerodix@gmail.com>
 # Licensed under the GNU Public License, version 3.
 
-
 # regular colors
 black="\e[0;30m"    # black
 red="\e[0;31m"    # red
@@ -12,35 +11,21 @@ magenta="\e[0;35m"    # magenta
 cyan="\e[0;36m"    # cyan
 white="\e[0;37m"    # white
 
-# emphasized (bolded) colors
-bblack="\e[1;30m"
-bred="\e[1;31m"
-bgreen="\e[1;32m"
-byellow="\e[1;33m"
-bblue="\e[1;34m"
-bmagenta="\e[1;35m"
-bcyan="\e[1;36m"
-bwhite="\e[1;37m"
+reverse="\e[7m"
 
-# reverse colors
-rblack="\e[7;30m"
-rred="\e[7;31m"
-rgreen="\e[7;32m"
-ryellow="\e[7;33m"
-rblue="\e[7;34m"
-rmagenta="\e[7;35m"
-rcyan="\e[7;36m"
-rwhite="\e[7;37m"
+if [ "$TERM" = "xterm" ]; then
+	red="\e[0;91m"    # red
+fi
 
 
-h1=$yellow
+h1=
 
 e=$red
 ok=$green
 wa=$yellow
 
-cela=$cyan
-ceta=$rcyan
+cela=$magenta
+ceta="${magenta}${reverse}"
 
 b="\e[0m\e[1m"
 bb=$green
--- scandvd.sh
+++ scandvd.sh
@@ -9,7 +9,7 @@ p=$(dirname $(readlink -f $0)); . $p/lib.sh
 
 echo -e "${h1}{( --- scandvd.sh $version --- )}${r}"
 
-usage="Usage:  ${b}scandvd.sh ${r}[${b}-d ${bb}/dev/dvd${r} | ${b}-q ${bb}/path${r} | ${b}-i ${bb}disc.iso${r}]\n
+usage="Usage:  ${b}scandvd.sh ${r}[${b}-d ${bb}/dev/dvd${r} | ${b}-q ${bb}/path${r} | ${b}-i ${bb}disc.iso${r}]
   -d   dvd device to read from (default is ${bb}/dev/dvd${r})
   -q   dvd directory to read from
   -i   dvd iso image to read from"
